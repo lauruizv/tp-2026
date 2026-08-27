f13d4205169f1d98d5e34b4dc85e11405ee283cb
Laureano Ruiz <lauruiz@frba.utn.edu.ar>
2026-08-27 14:49:58 -0300

    Update ventas.cpp

--- ventas.cpp
+++ ventas.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 int main(){
 
+  char fechDia[11];
+  char nomArchi[50]="comandas_XXXXXXXXXX.dat";
+  
 
+  cout<<"Ingrese la fecha de hoy (DD-MM-AAAA): "; cin>>fechDia;
 
+//condicion para cargar el nombre del archivo y asegurarse de no meter un caracter nulo de la fecha al nombre del archivo
+  for(int i=0;i<10 && fechDia[i]!='\0';i++){
+    nomArchi[9+i]=fechaDia[i];
+  }
 
 
 
