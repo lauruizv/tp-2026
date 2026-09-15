70a7b3a137ad2a564099cec1240ef7c1456078af
Laureano Ruiz <lauruiz@frba.utn.edu.ar>
2026-09-15 16:50:12 -0300

    Improve date input prompt and file handling
    Fixed input prompt formatting and added file handling for opening/creating a file.

--- ventas.cpp
+++ ventas.cpp
@@ -6,13 +6,18 @@ int main(){
   char nomArchi[50]="comandas_XXXXXXXXXX.dat";
   
 
-  cout<<"Ingrese la fecha de hoy (DD-MM-AAAA): "; cin>>fechDia;
+  cout<<" Ingrese la fecha de hoy (DD-MM-AAAA): "; cin>>fechDia;
 
 //condicion para cargar el nombre del archivo y asegurarse de no meter un caracter nulo de la fecha al nombre del archivo
   for(int i=0;i<10 && fechDia[i]!='\0';i++){
     nomArchi[9+i]=fechaDia[i];
   }
-
+//Apertura archivo/crear en caso de no existir
+FILE* archivoDia = fopen(nomArchi, "ab");
+    if (archivoDia == NULL) {
+        cout << "Error al crear/abrir el archivo." << endl;
+        return 1;
+    }
 
 
 
