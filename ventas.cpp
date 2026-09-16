30c85c43c78b3cf54d22187f07679bb5455e37f8
Laureano Ruiz <lauruiz@frba.utn.edu.ar>
2026-09-16 01:55:12 -0300

    Update ventas.cpp

--- ventas.cpp
+++ ventas.cpp
@@ -18,7 +18,26 @@ FILE* archivoDia = fopen(nomArchi, "ab");
         cout << "Error al crear/abrir el archivo." << endl;
         return 1;
     }
-
+//ciclo de carga de ventas
+  int IdMozo;
+  cout<<"Ingrese el ID del mozo: (Ingrese 0 para terminar el dia) "; cin>>IdMozo;
+  while(IdMozo!=0){
+    char claveIngresada[20];
+    cout<<"Ingrese la clave del mozo: "; cin>>claveIngresada;
+    //Aplicar corrimiento a la clave 
+    const  int numCorrimiento=3;
+    for(int i=0;i<3;i++){
+      claveIngresada[i]=claveIngresada[i]+numCorrimiento;
+    }
+    //Apertura de archivo mozos
+    FILE* archMozos = fopen("mozos.dat", "rb");
+    if(archMozos==NULL){
+      cout<<"Error, no se pudo abrir el archivo de mozos"<<endl;
+      return 1;
+    }
+    bool mozEnc=false;
+    bool clavCorr=false;
+    //recorrido con while fread 
 
 
 
